51b467c4e67458514601761ab660148a998fa9f4
Věra Staňková <stankova.verka@gmail.com>
2020-11-01 15:30:05 +0100

    komponenta Layer v separátní složce

---
+++ src/Layer/index.js
@@ -0,0 +1,12 @@
+const Layer = (props) => {
+  return `<div class="layer">
+  <div
+    class="layer__color"
+    style="background-color: ${props.color}"
+  ></div>
+  <div class="layer__label">${props.label}</div>
+  </div>
+  `;
+};
+
+export default Layer;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './index.html';
 import './style.css';
+import Layer from './Layer';
 
 console.log('funguju!');
 
+/* navigace */
+
 const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll('nav a');
-let ordered = false;
 
 document.querySelector('#nav-btn').addEventListener('click', () => {
   nav.classList.toggle('nav-closed');
@@ -20,6 +22,10 @@ navLinks.forEach((navLink) => {
 const drinkCup = document.querySelector('.drink__cup');
 let orderButton = document.querySelector('.order-btn');
 
+/* objednávka:  */
+
+let ordered = false;
+
 orderButton.addEventListener('click', () => {
   drinkCup.classList.toggle('drink__cup--selected');
   if (ordered === true) {
@@ -30,3 +36,20 @@ orderButton.addEventListener('click', () => {
     orderButton.textContent = 'Zrušit';
   }
 });
+
+/* ingredience jako komponenty */
+
+const ingredients = [
+  { color: '#feeeca', label: 'mléčná pěna' },
+  { color: '#fed7b0', label: 'teplé mléko' },
+  { color: '#613916', label: 'espresso' },
+];
+
+const drinkInfo = document.querySelector('.drink__info');
+
+drinkInfo.innerHTML += Layer(ingredients[0]);
+
+/* const drinkInfo = document.querySelector(".drink__info")
+const ingredients = [
+
+] */
