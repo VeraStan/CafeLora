c0931a3b3128e0a023630ff29efde56613da8608
Věra Staňková <stankova.verka@gmail.com>
2020-11-01 11:50:50 +0100

    responzivní menu

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const nav = document.querySelector('nav');
+const navlinks = document.querySelectorAll('nav a');
+
+document.querySelector('#nav-btn').addEventListener('click', () => {
+  nav.classList.toggle('nav-closed');
+});
+
+navlinks.forEach((navlink) => {
+  navlink.addEventListener('click', () => {
+    nav.classList.toggle('nav-closed');
+  });
+});
