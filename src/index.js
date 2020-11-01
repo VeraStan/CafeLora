89641e577bdc547de1e18caac75c2ac790cba31a
Věra Staňková <stankova.verka@gmail.com>
2020-11-01 12:23:32 +0100

    funkční objednávání

--- src/index.js
+++ src/index.js
@@ -4,14 +4,29 @@ import './style.css';
 console.log('funguju!');
 
 const nav = document.querySelector('nav');
-const navlinks = document.querySelectorAll('nav a');
+const navLinks = document.querySelectorAll('nav a');
+let ordered = false;
 
 document.querySelector('#nav-btn').addEventListener('click', () => {
   nav.classList.toggle('nav-closed');
 });
 
-navlinks.forEach((navlink) => {
-  navlink.addEventListener('click', () => {
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
     nav.classList.toggle('nav-closed');
   });
+});
+
+const drinkCup = document.querySelector('.drink__cup');
+let orderButton = document.querySelector('.order-btn');
+
+orderButton.addEventListener('click', () => {
+  drinkCup.classList.toggle('drink__cup--selected');
+  if (ordered === true) {
+    ordered = false;
+    orderButton.textContent = 'Objednat';
+  } else {
+    ordered = true;
+    orderButton.textContent = 'Zrušit';
+  }
 });
